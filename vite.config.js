63261b11492769b48d7f1dba8d94ce6265ff2937
wuxu1128 <xing255161@gmail.com>
2023-11-01 01:23:41 +0800

    resolve 404 problem

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,29 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { join, parse, resolve } from "path";
 
+function entryPoints(...paths) {
+  const entries = paths.map(parse).map(entry => {
+    const { dir, base, name, ext } = entry;
+    const key = join(dir, name);
+    const path = resolve(__dirname, dir, base);
+    return [key, path];
+  });
+
+  const config = Object.fromEntries(entries);
+  return config;
+}
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: entryPoints(
+        "index.html",
+        "404.html",
+      ),
+    },
+  },
   // server: {
   //   proxy: {
   //     "/api": {
