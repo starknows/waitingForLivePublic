47850744243c2e74f35c929da7cef4c95b2dfbfb
Cheng-An Ko <gary@kingone-design.com>
2023-11-03 10:27:41 +0800

    block page

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/:?',
+      path: '/:channelId?',
       name: 'Guide',
       component: () => import('../views/Guide.vue')
     },
